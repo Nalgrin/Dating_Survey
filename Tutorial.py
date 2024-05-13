aa8673f7fac3d733f4a635372646d4fbd8dd403b
Nalgrin <gestolle@gmail.com>
2024-05-12 23:29:09 -0400

    Dating Survey

---
+++ Tutorial.py
@@ -0,0 +1,87 @@
+#title
+print("Dating Survey")
+
+#Basic Imformation
+name = input("What is your name? ")
+age = input("What's your age? ")
+gender = input("Are you Male or Female? ")
+sexual_orientation = input("What's your sexual Prefrence? ")
+Current_Relationship_Status = input("What's your current relationship status? ")
+
+#Daiting Prefrences
+age_range = input("What's your age range? ") 
+Type_Relationship = input("What type of relationship are you looking for? ")
+type_person = input("Do you perfer somone to have the same intrests or have diffrent from you? ")
+cultural_background = input("Are you open to daiting people with diffrent cultural backgrounds? ")
+
+#Personality - Lifestyle
+personality = input("Describe your personality. ")
+intrests = input("What are your hobbies and intrests? ")
+activities = input("Do you like outdoor, indoor activiities, or a mix of both? ")
+morning_night = input("Are you a morining person or a night owl?")
+
+#Relationship Expectations
+expectations = input("What are your current expectations from a partner? ")
+communication = input("Is communication importnat to you in a relationship")
+relationship_pace = input("Are you wanting to settle down in the near future or take things slow? ")
+
+#Compatiblility - Values
+values = input("What are your important characteristics in a relationship? ")
+deal_breakers = input("Do you have any deal breakers, if so what are they? ")
+attraction = input("Do you require physical attraction? ")
+
+#Daiting Experience
+experience = input("Overall what has your best dating experience? ")
+challenge = input("What has been your overall biggest challenge in past relationships? ")
+longest_relationship = input("What is your longest relationship? ")
+
+#Future Goals
+future = input("Where do you see yourself in 5 years? ")
+children = input("How many kids do you want and if so, how many? ")
+relocate = input("Are you open to relocate for your parnter? ")
+marriage = input("Will you want marriage at some point? ")
+
+#Communication and Compatibility
+conflict = input("How would you handle conflic in a relationship?")
+beliefs = input("Is it important for your partner to have similar religious beliefs? ")
+communicate = input("What is your perferred method of communicating? ")
+
+
+#Paragraph
+print("Basic Imformation")
+print("Hi my name is " + name + ", I am " + age + ". I am " + sexual_orientation + ", with my relationship staus being " + Current_Relationship_Status + ".")
+print()
+print("Daiting Prefrences")
+print("I am looking for somone who is around" + age_range + ", the type of relationship I want is " + Type_Relationship + ". I will date somone who will have " + type_person + " intrests. But I am " + cultural_background + "to someone who has a cultural background")
+print()
+print("Personality - Lifestyle")
+print("Personality: " + personality + ".")
+print("Intrests: " + intrests + ".")
+print("Activities: " + activities + ".")
+print("Morning or Night: " + morning_night + ".")
+print()
+print("Relationship Expectaions")
+print("Expectations: " + expectations + ".")
+print("Communication: " + communication + ".")
+print("Pace: " + relationship_pace + ".")
+print()
+print("Compatiblility - Values")
+print("Characteristics: " + values + ".")
+print("Deal Breakers: " + deal_breakers + ".")
+print("Physical Attraction: " + attraction)
+print()
+print("Daiting Experience")
+print("Best Dating Experience: " + experience + ".")
+print("Past Relationship Challenge: " + challenge + ".")
+print("Longest Relationship: " + longest_relationship)
+print()
+print("Future Goals")
+print("Myself in 5 Years: " + future + ".")
+print("Kids: " + children + ".")
+print("Relocating: " + relocate + ".")
+print("Marriage: " + marriage + ".")
+print()
+print("Communication and Compatibility")
+print("Conflict: " + conflict + ".")
+print("Belifs: " + beliefs + ".")
+print("Communication: " + communicate + ".")
